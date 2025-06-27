cff7e9d0eb163065c2e70283d89b32749a9d5cfe
Makesh <makeshmr2409@gmail.com>
2025-06-27 20:34:57 +0530

    about section new changes

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -18,7 +18,7 @@ const devopsSkills = [
         <section id="about"  className="min-h-screen flex items-center justify-center py-20" >
 
         <div className="max-w-3xl mx-auto px-4">
-            <h2 className="text-3xl font-bold mb-8 bg-gradient from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center"> 
+            <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center"> 
                 {" "}
                 About Me 
             </h2>
@@ -28,6 +28,8 @@ const devopsSkills = [
                     With hands-on experience in CI/CD pipelines, infrastructure as code, and monitoring solutions, I focus on optimizing performance and ensuring high availability across cloud environments. 
                     I'm passionate about automation, developer experience, and continuously enhancing systems to support fast, efficient, and high-quality software delivery.
                 </p>
+                
+                </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
                        <h3 className="text-xl font-bold mb-4">Cloud</h3> 
@@ -42,7 +44,7 @@ const devopsSkills = [
                             </span>
                             ))}
                             </div>
-                       </div>
+
                     <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
                        <h3 className="text-xl font-bold mb-4">Linux</h3> 
                        <div className="flex flex-wrap gap-2">
@@ -72,6 +74,42 @@ const devopsSkills = [
                         </div>
                     </div> 
                 </div>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+                <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4">🏫 Education</h3>
+                            <ul className="list-disc list-inside text-gray-300 space-y-2 ">
+                                <list>
+                                    <strong> B.Tech in Electrical & Electronics Engineering</strong> - NIT Calicut (2017-2021)
+                                </list>
+                                <li>
+                                    Relevant Coursework: Data Structures & Algorithm, Devops, Cloud Computing
+                                </li>
+                            </ul>
+                        
+                </div>
+                
+                <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4">💼 Work Experience</h3>
+                            <div className="space-y-4 text-gray-300">
+                                <h4 className="font-semibold"> Systems Engineer at Sify Technologies</h4> (Aug 2022 - Jan 2025)
+                                <p>Managed and secured Linux systems (RHEL, Ubuntu, Oracle Linux, Amazon Linux) across physical, virtual, and cloud environments.
+
+</p>
+<p>Automated configuration management, patching, and deployments using Ansible, Bash, and Docker, reducing manual effort and boosting efficiency.
+
+</p>
+
+                            </div>
+                            <div className="space-y-4 text-gray-300">
+                                <h4 className="font-semibold"> Engineer Trainee at Sify Technologies</h4> (Aug 2021 - July 2022)
+                                <p></p>
+
+                            </div>
+                        
+                </div>
+
             </div>
 
         </div>
