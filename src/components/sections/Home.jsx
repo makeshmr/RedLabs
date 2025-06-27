4df7203b6f6bee4bc80ba68a56169d464368fb81
Makesh <makeshmr2409@gmail.com>
2025-06-27 17:03:58 +0530

    mobile menu

---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,18 @@
+
+
+export const Home = () => {
+    return <section 
+    id='home' 
+    className="min-h-screen flex items-center justify center relative"
+    >
+        <div className="text-center z-10 px-4">
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">
+                Hi, I'm Makesh 
+            </h1>
+        </div>
+
+
+
+
+    </section>
+}
