fd43058a033c884038f35069064c8a1b842b465e
Makesh <makeshmr2409@gmail.com>
2025-06-27 16:41:42 +0530

    mobile menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { LoadingScreen } from "./components/LoadingScreen";
 import "./index.css"
 import { Navbar } from "./components/Navbar";
+import { MobileMenu } from "./components/MobileMenu";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -12,12 +13,13 @@ function App() {
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}
       <div 
-        className={`min-h-screen transition-opacity duration-700 ${
+      className={`min-h-screen transition-opacity duration-700 ${
         isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
     
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       </div>
     </>
   );
