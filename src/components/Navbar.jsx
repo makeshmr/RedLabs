14b220c72f59340dee848fbeab4859c880b33df0
Makesh <makeshmr2409@gmail.com>
2025-06-27 13:34:13 +0530

    home sectino in navigatin bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,8 @@ export const Navbar = () => {
     return <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
         <div className="max-w-5xl mx-auto px-4">
             <div className="flex justify-between items-center h-16">
-                <a href="#home" className="font-mono text-xl font-bold text-white">
+                <a 
+                    href="#home" className="font-mono text-xl font-bold text-white">
                     {" "}
                     makesred<span className="text-blue-500">.lab</span>
                 </a>
@@ -11,11 +12,33 @@ export const Navbar = () => {
                     &#9776;
                 </div>
                 <div className="hidden md:flex items-center space-x-8">
-                    <a href="#home" 
-                    className="tex-gray-300 hove: text-white transition-colors"
+                    <a 
+                        href="#home" 
+                        className="tex-gray-300 hove: text-white transition-colors"
                     >
-                    
-
+                        {" "}
+                        Home{" "}
+                    </a>
+                     <a 
+                        href="#about" 
+                        className="tex-gray-300 hove: text-white transition-colors"
+                    >
+                        {" "}
+                        About{" "}
+                    </a>
+                     <a 
+                        href="#projects" 
+                        className="tex-gray-300 hove: text-white transition-colors"
+                    >
+                        {" "}
+                        Projects{" "}
+                    </a>
+                     <a 
+                        href="#contact" 
+                        className="tex-gray-300 hove: text-white transition-colors"
+                    >
+                        {" "}
+                        Contact{" "}
                     </a>
 
                 </div>
