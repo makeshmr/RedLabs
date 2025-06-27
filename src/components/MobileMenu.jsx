5b28146e8c95f0014199d94165a0f2fd9cc6d18d
Makesh <makeshmr2409@gmail.com>
2025-06-27 16:51:08 +0530

    mobile menu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -15,10 +15,61 @@ export const MobileMenu = ({ menuOpen,setMenuOpen}) => {
             <button 
                 onClick={() => setMenuOpen(false)} 
                 className="absolute top-6 right-6 text-white text-3xl focus:outline-none cursor-pointer">
+                aria-label="Close Menu"
                 &times;
             </button>
 
+                   <a 
+                        href="#home" 
+                        onClick={() => setMenuOpen(false)}
+                        className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
+                                    ${menuOpen 
+                                        ? "opacity-100 translate-y-0" 
+                                        : "opacity-0 translate-y-5"}
+                                `}
+                    >
+    
+                        Home
+                    </a>
+                     <a 
+                        href="#about"
+                        onClick={() => setMenuOpen(false)} 
+                        className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
+                                    ${menuOpen 
+                                        ? "opacity-100 translate-y-0" 
+                                        : "opacity-0 translate-y-5"}
+                                `}
+                    >
+                        
+                        About
+                    </a>
+                     <a 
+                        href="#projects" 
+                        onClick={() => setMenuOpen(false)}
+                        className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
+                                    ${menuOpen 
+                                        ? "opacity-100 translate-y-0" 
+                                        : "opacity-0 translate-y-5"}
+                                `}
+                    >
+                        
+                        Projects
+                    </a>
+                     <a 
+                        href="#contact" 
+                        onClick={() => setMenuOpen(false)}
+                        className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
+                                    ${menuOpen 
+                                        ? "opacity-100 translate-y-0" 
+                                        : "opacity-0 translate-y-5"}
+                                `}
+                    >
+                        
+                        Contact
+                    </a>
 
+
+        
         </div>
     )
 }
