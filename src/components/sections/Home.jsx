773d91b6c4e4d7abc102cc1e224f6b21cec8734d
Makesh <makeshmr2409@gmail.com>
2025-06-27 17:31:17 +0530

    home page completed

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -22,7 +22,7 @@ export const Home = () => {
                 className="bg-blue-500 text-white py-3 px- 6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 
                 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
                 >
-                 View Projects
+                   Projects
 
                 </a>
 
