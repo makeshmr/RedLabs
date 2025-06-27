dca31f76fab1c52856b8776ec266ee443ddd1214
Makesh <makeshmr2409@gmail.com>
2025-06-27 17:17:11 +0530

    adding home part

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -9,6 +9,13 @@ export const Home = () => {
             <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-tight">
                 Hi, I'm Makesh 
             </h1>
+
+            <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+                I'm a DevOps/Site Reliability Engineer with expertise in Linux, AWS, and Azure, along with a strong command of DevOps tools and automation practices. Experienced in building and managing scalable, reliable infrastructure using Docker, Kubernetes, and Terraform.
+                Skilled in CI/CD pipelines, monitoring, and infrastructure as code, with a focus on enhancing performance and uptime across cloud environments. Dedicated to creating systems that are not only stable but also fast and user-friendly.
+                Passionate about automation, developer experience, and continuously improving infrastructure to support efficient, high-quality software delivery.
+
+            </p>
         </div>
 
 
