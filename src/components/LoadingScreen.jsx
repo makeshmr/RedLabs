a97a9a8459596a723ad80e68a6ed3de3b9eb9f12
Makesh <makeshmr2409@gmail.com>
2025-06-28 12:31:06 +0530

    adding hello world to loading screen

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -29,7 +29,7 @@ export const LoadingScreen = ({ onComplete }) => {
     return (
         <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
             <div className="mb-3 text-4xl font-mono font-bold">
-                <span className="animate-blink ml-1"> | </span>
+               < Hello World /> <span className="animate-blink ml-1"> | </span>
             </div>
     
 
