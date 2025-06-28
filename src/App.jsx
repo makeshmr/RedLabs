3f2ee9e9c43c4533350df4f4d4305b772f561614
Makesh <makeshmr2409@gmail.com>
2025-06-28 14:58:18 +0530

    project1 added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
 import { Contact } from "./components/sections/Contact";
-
+import { Projects } from "./components/sections/Projects";
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
@@ -25,6 +25,7 @@ function App() {
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
         <About />
+        <Projects />
         <Contact />
       </div>
     </>
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -13,10 +13,24 @@ export const Projects = () => {
                 Featured Projects
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <h3>Data Analytics</h3>
+                <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hver:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+                <h3>Exploratory Data Analysis Case Study : Rise in Accidents Cases in USA </h3>
                 <p>
-                    
+                    Analyzed 1.3M+ US accident records using Pandas and visualized trends with Seaborn, Plotly, Matplotlib, and Folium.
+                     Found a significant rise in accidents post-2020, linked to increased driver aggression amid growing traffic congestion.
                 </p>
+                <div>
+                    {["Python", "Data Cleaning", "Data Analysis", "Data Visualization", "Exploratory Data Analysis (EDA)", "Statistical Analysis"].map((tech,key) => (
+                        <span 
+                            key={key}
+                            className="bg-blue-500/10 text-blue-500 py-1 px-3  rounded-full text-sm hover:bg-blue-500/20
+                            hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                                {tech}
+
+                            </span>
+                    ))}
+                </div>
+                </div>
             
             </div>
             
