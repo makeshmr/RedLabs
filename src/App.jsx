98bd51fe97abfed2b720ec6b6ad6da4c897fd81c
Makesh <makeshmr2409@gmail.com>
2025-06-28 12:47:30 +0530

    adding project components

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
+import { Contact } from "./components/sections/Contact";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -24,6 +25,7 @@ function App() {
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
         <About />
+        <Contact />
       </div>
     </>
   );
